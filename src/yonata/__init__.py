60698cc983efc49287e7337bc80af9b08c4e5f60
Erwin <erwinwingyonata@gmail.com>
2025-05-20 15:57:03 +0700

    completed tutorial to publish a package to pypi with poetry

---
+++ src/yonata/__init__.py
@@ -0,0 +1,1 @@
+from .printers import say_greeting
---
+++ tests/test_printers.py
@@ -0,0 +1,12 @@
+
+from yonata.printers import say_greeting
+
+def test_say_greeting(capsys):
+    # Call the function
+    say_greeting()
+
+    # Capture the output
+    captured = capsys.readouterr()
+
+    # Check if the output is as expected
+    assert captured.out == "Hello, World!\n"
